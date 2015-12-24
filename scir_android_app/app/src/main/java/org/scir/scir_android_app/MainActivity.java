7b95be9d3cdf9833c502113e2aadb50f1830f076
Smart City Infra Reporting <smartcityinfrareporting@gmail.com>
2015-12-25 03:10:03 +0530

    Added the WIP copy of android app capturing photograph of problem.

---
+++ scir_android_app/app/src/main/java/org/scir/scir_android_app/MainActivity.java
@@ -0,0 +1,133 @@
+package org.scir.scir_android_app;
+
+import android.os.Bundle;
+
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+    private static final int TAKE_PICTURE_REQUEST_B = 100;
+
+    private ImageView mCameraImageView;
+    private Bitmap mCameraBitmap;
+    private Button mSaveImageButton;
+
+    private OnClickListener mCaptureImageButtonClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            startImageCapture();
+        }
+    };
+
+    private OnClickListener mSaveImageButtonClickListener = new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            File saveFile = openFileForImage();
+            if (saveFile != null) {
+                saveImageToFile(saveFile);
+            } else {
+                Toast.makeText(MainActivity.this, "Unable to open file for saving image.",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCameraImageView = (ImageView) findViewById(R.id.camera_image_view);
+
+        findViewById(R.id.capture_image_button).setOnClickListener(mCaptureImageButtonClickListener);
+
+        mSaveImageButton = (Button) findViewById(R.id.save_image_button);
+        mSaveImageButton.setOnClickListener(mSaveImageButtonClickListener);
+        mSaveImageButton.setEnabled(false);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == TAKE_PICTURE_REQUEST_B) {
+            if (resultCode == RESULT_OK) {
+                // Recycle the previous bitmap.
+                if (mCameraBitmap != null) {
+                    mCameraBitmap.recycle();
+                    mCameraBitmap = null;
+                }
+                Bundle extras = data.getExtras();
+ //               mCameraBitmap = (Bitmap) extras.get("data");
+                byte[] cameraData = extras.getByteArray(CameraActivity.EXTRA_CAMERA_DATA);
+                if (cameraData != null) {
+                    mCameraBitmap = BitmapFactory.decodeByteArray(cameraData, 0, cameraData.length);
+                    mCameraImageView.setImageBitmap(mCameraBitmap);
+                    mSaveImageButton.setEnabled(true);
+                }
+            } else {
+                mCameraBitmap = null;
+                mSaveImageButton.setEnabled(false);
+            }
+        }
+    }
+
+    private void startImageCapture() {
+        startActivityForResult(new Intent(MainActivity.this, CameraActivity.class), TAKE_PICTURE_REQUEST_B);
+    }
+
+    private File openFileForImage() {
+        File imageDirectory = null;
+        String storageState = Environment.getExternalStorageState();
+        if (storageState.equals(Environment.MEDIA_MOUNTED)) {
+            imageDirectory = new File(
+                    Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
+                    "com.oreillyschool.android2.camera");
+            if (!imageDirectory.exists() && !imageDirectory.mkdirs()) {
+                imageDirectory = null;
+            } else {
+                SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy_mm_dd_hh_mm",
+                        Locale.getDefault());
+
+                return new File(imageDirectory.getPath() +
+                        File.separator + "image_" +
+                        dateFormat.format(new Date()) + ".png");
+            }
+        }
+        return null;
+    }
+
+    private void saveImageToFile(File file) {
+        if (mCameraBitmap != null) {
+            FileOutputStream outStream = null;
+            try {
+                outStream = new FileOutputStream(file);
+                if (!mCameraBitmap.compress(Bitmap.CompressFormat.PNG, 100, outStream)) {
+                    Toast.makeText(MainActivity.this, "Unable to save image to file.",
+                            Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Saved image to: " + file.getPath(),
+                            Toast.LENGTH_LONG).show();
+                }
+                outStream.close();
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "Unable to save image to file.",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
